feb9306b3664eaf6888e7f3254925f1f9604a37c
GNooNJo <jjjchoon@nate.com>
2024-02-06 16:16:39 +0900

    init: redux

--- src/redux/modules/calculator.js
+++ src/redux/modules/calculator.js
@@ -4,7 +4,12 @@ const initialState = {
 };
 
 // 리듀서
-const calculator = (state = initialState, action) => {};
+const calculator = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
 
 // 모듈파일에서는 리듀서를 export default 한다.
 export default calculator;
